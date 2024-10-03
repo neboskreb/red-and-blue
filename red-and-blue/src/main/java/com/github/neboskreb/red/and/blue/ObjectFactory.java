5419ecc721b21128ac487089600255e275afb63b
John Y. Pazekha <neboskreb@narod.ru>
2024-10-03 02:31:55 +0200

    Adapted the object creation to the "Vintage" version of the refactored internal API in Equals Verifier

--- red-and-blue/src/main/java/com/github/neboskreb/red/and/blue/ObjectFactory.java
+++ red-and-blue/src/main/java/com/github/neboskreb/red/and/blue/ObjectFactory.java
@@ -1,40 +1,46 @@
 package com.github.neboskreb.red.and.blue;
 
-import nl.jqno.equalsverifier.internal.prefabvalues.FactoryCache;
-import nl.jqno.equalsverifier.internal.prefabvalues.JavaApiPrefabValues;
-import nl.jqno.equalsverifier.internal.prefabvalues.PrefabValues;
-import nl.jqno.equalsverifier.internal.prefabvalues.TypeTag;
-import nl.jqno.equalsverifier.internal.reflection.ClassAccessor;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import nl.jqno.equalsverifier.internal.reflection.FactoryCache;
+import nl.jqno.equalsverifier.internal.reflection.JavaApiPrefabValues;
+import nl.jqno.equalsverifier.internal.reflection.TypeTag;
+import nl.jqno.equalsverifier.internal.reflection.instantiation.VintageValueProvider;
+import nl.jqno.equalsverifier.internal.reflection.vintage.ClassAccessor;
 import nl.jqno.equalsverifier.internal.util.PrefabValuesApi;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 
 class ObjectFactory {
     private static final FactoryCache BASIC_JAVA_PREFABS = JavaApiPrefabValues.build();
 
-    private final PrefabValues prefabValues;
+    private final Objenesis objenesis;
+    private final VintageValueProvider valueProvider;
 
     public ObjectFactory(List<RedAndBlue<?>> prefabs) {
+        objenesis = new ObjenesisStd();
         FactoryCache factoryCache = new FactoryCache();
         addPrefabs(prefabs, factoryCache);
         FactoryCache cache = BASIC_JAVA_PREFABS.merge(factoryCache);
-        prefabValues = new PrefabValues(cache);
+        valueProvider = new VintageValueProvider(cache, objenesis);
     }
 
     private <T> void addPrefabs (List<RedAndBlue<?>> prefabs, FactoryCache factoryCache) {
         for (RedAndBlue<?> pr : prefabs) {
             @SuppressWarnings("unchecked")
             RedAndBlue<T> prefab = (RedAndBlue<T>) pr;
-            PrefabValuesApi.addPrefabValues(factoryCache, prefab.clazz(), prefab.red(), prefab.blue());
+            PrefabValuesApi.addPrefabValues(factoryCache, objenesis, prefab.clazz(), prefab.red(), prefab.blue());
         }
     }
 
     public <T> T  create(Class<T> type, COLOR color) {
-        ClassAccessor<T> classAccessor = ClassAccessor.of(type, prefabValues);
+        ClassAccessor<T> classAccessor = ClassAccessor.of(type, valueProvider, objenesis);
         TypeTag typeTag = new TypeTag(type);
         switch (color) {
-            case RED:  return classAccessor.getRedObject(typeTag);
-            case BLUE: return classAccessor.getBlueObject(typeTag);
+            case RED:  return classAccessor.getRedObject(typeTag, new LinkedHashSet<>());
+            case BLUE: return classAccessor.getBlueObject(typeTag, new LinkedHashSet<>());
             default: throw new UnsupportedOperationException("Not (yet) implemented for: " + color);
         }
     }
