f6dd16ec39026b93af795df113daff4e532d2207
John Y. Pazekha <neboskreb@narod.ru>
2025-11-01 16:07:15 +0100

    Supporting JUnit versions 5 and 6 simultaneously

---
+++ regression/junit5/src/test/java/com/github/neboskreb/red/and/blue/junit5/MyObject.java
@@ -0,0 +1,13 @@
+package com.github.neboskreb.red.and.blue.junit5;
+
+public final class MyObject {
+    private final String value;
+
+    MyObject(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
