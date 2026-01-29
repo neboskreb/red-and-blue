8f4139945f1bc4d120b40acacb49145763d8d842
John Y. Pazekha <neboskreb@narod.ru>
2026-01-29 11:52:54 +0100

    Added the missing test

---
+++ regression/android/src/test/java/com/github/neboskreb/red/and/blue/junit6/AndroidJunitSmokeTest.java
@@ -0,0 +1,40 @@
+package com.github.neboskreb.red.and.blue.junit6;
+
+import com.github.neboskreb.red.and.blue.RedAndBlueExtension;
+import com.github.neboskreb.red.and.blue.annotation.BlueInstance;
+import com.github.neboskreb.red.and.blue.annotation.PrefabBlue;
+import com.github.neboskreb.red.and.blue.annotation.PrefabRed;
+import com.github.neboskreb.red.and.blue.annotation.RedInstance;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(RedAndBlueExtension.class)
+class AndroidJunitSmokeTest {
+
+    @BlueInstance
+    protected MyObject blueField;
+    @RedInstance
+    private MyObject redField;
+
+    @PrefabRed
+    protected String prefabRedString = "red";
+
+    @PrefabBlue
+    private static String baseStaticPrefabBlueString() {
+        return "blue";
+    }
+
+    @Test
+    void testParameterInjection(@RedInstance MyObject red, @BlueInstance MyObject blue) {
+        assertEquals("red", red.value());
+        assertEquals("blue", blue.value());
+    }
+
+    @Test
+    void testFieldInjection() {
+        assertEquals("red", redField.value());
+        assertEquals("blue", blueField.value());
+    }
+}
